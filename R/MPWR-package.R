907c812626555f8310228572874782e00125152e
Faïcel Chamroukhi <faicel.chamroukhi@gmail.com>
2019-08-06 10:05:47 +0200

    update manual and other minor stuff

--- R/MPWR-package.R
+++ R/MPWR-package.R
@@ -2,8 +2,7 @@
 ## usethis namespace: start
 #' @useDynLib MPWR, .registration = TRUE
 ## usethis namespace: end
-NULL
 ## usethis namespace: start
 #' @importFrom Rcpp sourceCpp
 ## usethis namespace: end
-NULL
+"_PACKAGE"
